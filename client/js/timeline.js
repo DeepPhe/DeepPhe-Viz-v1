edae0b6417b19ffab1e8fc295b8396baccd49ac5
Zhou Yuan <yuanzhou19@gmail.com>
2017-08-30 12:00:29 -0400

    New timeline design

--- client/js/timeline.js
+++ client/js/timeline.js
@@ -1,117 +1,205 @@
-// Timeline
+// Data
 
-//data
 var lanes = ["Radiology Report", "Progress Note", "Surgical Pathology Report", "Discharge Summary"];
 
-var laneLength = lanes.length;
+var data = [
+ {
+   "report_id": 1,
+   "report_time": "2014-04-05",
+   "report_type": 0
+ },
+ {
+   "report_id": 2,
+   "report_time": "2014-12-04",
+   "report_type": 0
+ },
+ {
+   "report_id": 3,
+   "report_time": "2015-02-05",
+   "report_type": 0
+ },
+ {
+   "report_id": 4,
+   "report_time": "2015-03-06",
+   "report_type": 1
+ },
+ {
+   "report_id": 5,
+   "report_time": "2015-08-16",
+   "report_type": 1
+ },
+ {
+   "report_id": 6,
+   "report_time": "2015-08-26",
+   "report_type": 1
+ },
+ {
+   "report_id": 7,
+   "report_time": "2015-09-10",
+   "report_type": 2
+ },
+ {
+   "report_id": 8,
+   "report_time": "2015-10-22",
+   "report_type": 2
+ },
+ {
+   "report_id": 9,
+   "report_time": "2015-12-03",
+   "report_type": 2
+ },
+ {
+   "report_id": 10,
+   "report_time": "2016-01-19",
+   "report_type": 3
+ },
+ {
+   "report_id": 11,
+   "report_time": "2016-03-11",
+   "report_type": 3
+ },
+ {
+   "report_id": 12,
+   "report_time": "2016-11-24",
+   "report_type": 3
+ }
+];
 
-var items = [
-				{"type": 0, "id": "Report1", "time": 5},
-				{"type": 0, "id": "Report2", "time": 265},
-				{"type": 0, "id": "Report3", "time": 580},
-				{"type": 0, "id": "Report4", "time": 620},
-				{"type": 0, "id": "Report5", "time": 960},
-				{"type": 1, "id": "Report6", "time": 1270},
-				{"type": 1, "id": "Report7", "time": 1370},
-				{"type": 1, "id": "Report8", "time": 1645},
-				{"type": 1, "id": "Report9", "time": 300},
-				{"type": 1, "id": "Report10", "time": 550},
-				{"type": 1, "id": "Report11", "time": 710},
-				{"type": 2, "id": "Report12", "time": 800},
-				{"type": 2, "id": "Report13", "time": 1190},
-				{"type": 2, "id": "Report14", "time": 1340},
-				{"type": 2, "id": "Report15", "time": 1610},
-				{"type": 2, "id": "Report16", "time": 1870},
-				{"type": 2, "id": "Report17", "time": 1910},
-				{"type": 2, "id": "Report18", "time": 1925},
-				{"type": 3, "id": "Report19", "time": 1990},
-				{"type": 3, "id": "Report20", "time": 10},
-				{"type": 3, "id": "Report21", "time": 690},
-				{"type": 3, "id": "Report22", "time": 920},
-				{"type": 3, "id": "Report23", "time": 1390},
-				{"type": 3, "id": "Report24 Empire", "time": 1900}
-			];
+console.log(data);
 
-var timeBegin = 0;
+var margin = {top: 20, right: 20, bottom: 100, left: 200};
+var width = 960 - margin.left - margin.right;
+var height = 320 - margin.top - margin.bottom;
 
-var timeEnd = 2000;
+var overviewMargin = {top: 250, right: 20, bottom: 30, left: 200};
+var overviewHeight = 320 - overviewMargin.top - overviewMargin.bottom;
 
-var miniRect = {width: 15, height: 10};
 
-var m = [20, 15, 15, 160]; //top right bottom left
-var w = 960 - m[1] - m[3];
-var h = 500 - m[0] - m[2];
-var miniHeight = laneLength * 12 + 50;
-var mainHeight = h - miniHeight - 50;
+var mainY = d3.scaleLinear()
+		.domain([0, lanes.length])
+		.range([0, height]);
+
+
+var parseTime = d3.timeParse("%Y-%m-%d");
+
+var x = d3.scaleTime()
+		.range([0, width]);
+
+var overviewX = d3.scaleTime()
+		.range([0, width]);
+
+var y = d3.scaleLinear()
+		.range([0, height]);
+
+var overviewY = d3.scaleLinear()
+		.range([0, overviewHeight]);
+
+// https://github.com/d3/d3-axis#axisBottom
+var xAxis = d3.axisBottom(x).tickSize(10);
+
+var yAxis = d3.axisLeft(y).tickSize(0);
+
+var overviewXAxis = d3.axisBottom(overviewX).tickSize(0);
+
+var brush = d3.brushX()
+    .extent([[0, 0], [width, overviewHeight]])
+    .on("brush", brushed);
+
+var zoom = d3.zoom()
+    .scaleExtent([1, Infinity])
+    .translateExtent([[0, 0], [width, height]])
+    .extent([[0, 0], [width, height]])
+    .on("zoom", zoomed);
+
+var svg = d3.select("#reports-timeline").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
+
+svg.append("defs").append("clipPath")
+    .attr("id", "clip")
+    .append("rect")
+    .attr("width", width)
+    .attr("height", height);
+
+// Main focus area
+var focus = svg.append("g")
+    .attr("class", "focus")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+// Mini overview
+var context = svg.append("g")
+    .attr("class", "context")
+    .attr("transform", "translate(" + overviewMargin.left + "," + overviewMargin.top + ")");
 
 // Up to 10 color categories for 10 types of reports
 var reportColor = d3.scaleOrdinal(d3.schemeCategory10);
 
-//scales
-var miniX = d3.scaleLinear()
-		.domain([timeBegin, timeEnd])
-		.range([0, w]);
+data.forEach(function(d) {
+	d.report_time = parseTime(d.report_time);
+});
 
-var miniY = d3.scaleLinear()
-		.domain([0, laneLength])
-		.range([0, miniHeight]);
+// The earliest report date
+var xMin = d3.min(data, function(d) { return d.report_time; });
 
-var mainX = d3.scaleLinear()
-		.range([0, w]);
+// The latest report date
+var xMax = d3.max(data, function(d) { return d.report_time; });
 
-var mainY = d3.scaleLinear()
-		.domain([0, laneLength])
-		.range([0, mainHeight]);
+x.domain([xMin, xMax]);
+y.domain([0, lanes.length]);
+
+overviewX.domain(x.domain());
+overviewY.domain(y.domain());
 
 
 
-var chart = d3.select("#reports-timeline")
-			.append("svg")
-			.attr("width", w + m[1] + m[3])
-			.attr("height", h + m[0] + m[2])
-			.attr("class", "chart");
+// append scatter plot to main chart area
+var messages = focus.append("g");
 
-chart.append("defs").append("clipPath")
-	.attr("id", "clip")
-	.append("rect")
-	.attr("width", w)
-	.attr("height", mainHeight);
+messages.attr("clip-path", "url(#clip)");
 
-// Main chart for zoom in details
-var main = chart.append("g")
-			.attr("transform", "translate(" + m[3] + "," + m[0] + ")")
-			.attr("width", w)
-			.attr("height", mainHeight)
-			.attr("class", "main");
+messages.selectAll("message")
+    .data(data)
+    .enter().append("circle")
+    .attr('class', 'message')
+    .attr("r", 4)
+    .attr("cx", function(d) { 
+    	return x(d.report_time); 
+    })
+    .attr("cy", function(d) { 
+    	return y(d.report_type); 
+    })
 
-// Mini overview chart
-var mini = chart.append("g")
-			.attr("transform", "translate(" + m[3] + "," + (mainHeight + m[0]) + ")")
-			.attr("width", w)
-			.attr("height", miniHeight)
-			.attr("class", "mini");
+focus.append("g")
+    .attr("class", "axis x-axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+
 
 //main lanes and texts
-main.append("g").selectAll(".laneLines")
-	.data(items)
+focus.append("g").selectAll(".laneLine")
+	.data(lanes)
 	.enter().append("line")
-	.attr("x1", m[1])
-	.attr("y1", function(d) {
-		return mainY(d.type);
+	.attr("x1", margin.right)
+	.attr("y1", function(d, i) {
+		return mainY(i);
 	})
-	.attr("x2", w)
-	.attr("y2", function(d) {
-		return mainY(d.type);
+	.attr("x2", width)
+	.attr("y2", function(d, i) {
+		return mainY(i);
 	})
 	.attr("stroke", "lightgray")
+	.attr("class", "laneLine");
 
-main.append("g").selectAll(".laneText")
+focus.append("g").selectAll(".laneText")
 	.data(lanes)
 	.enter().append("text")
 	.text(function(d) {
 		return d;
 	})
-	.attr("x", -m[1])
+	.attr("x", -margin.right)
 	.attr("y", function(d, i) {
 		return mainY(i + .5);
 	})
@@ -119,181 +207,95 @@ main.append("g").selectAll(".laneText")
 	.attr("text-anchor", "end")
 	.attr("class", "laneText");
 
-//mini lanes and texts
-mini.append("g").selectAll(".laneLines")
-	.data(items)
-	.enter().append("line")
-	.attr("x1", m[1])
-	.attr("y1", function(d) {
-		return miniY(d.type);
-	})
-	.attr("x2", w)
-	.attr("y2", function(d) {
-		return miniY(d.type);
-	})
-	.attr("stroke", "lightgray");
+// Show total reports count
+focus.append("text")
+    .attr("x", -margin.left)
+    .attr("dy", 0)
+    .attr("text-anchor", "start")
+    .text("Total reports: " + data.length);
 
-mini.append("g").selectAll(".laneText")
-	.data(lanes)
-	.enter().append("text")
-	.text(function(d) {
-		return d;
-	})
-	.attr("x", -m[1])
-	.attr("y", function(d, i) {
-		return miniY(i + .5);
-	})
-	.attr("dy", ".5ex")
-	.attr("text-anchor", "end")
-	.attr("class", "laneText");
+// X axis bottom text
+svg.append("text")
+    .attr("transform",
+          "translate(" + ((width + margin.right + margin.left)/2) + " ," +
+                         (height + margin.top + margin.bottom) + ")")
+    .style("text-anchor", "middle")
+    .text("Patient Reports Timeline");
 
-var itemRects = main.append("g")
-					.attr("clip-path", "url(#clip)");
+svg.append("rect")
+	.attr("class", "zoom")
+	.attr("width", width)
+	.attr("height", height)
+	.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+	.call(zoom);
 
-//mini item rects
-mini.append("g").selectAll("miniItems")
-	.data(items)
-	.enter().append("rect")
-	.attr("class", function(d) {
-		return "miniItem" + d.type;
+// append scatter plot to brush chart area
+var messages = context.append("g");
+
+messages.attr("clip-path", "url(#clip)");
+
+messages.selectAll("message")
+	.data(data)
+	.enter().append("circle")
+	.attr('class', 'messageContext')
+	.attr("r", 3)
+	.attr("cx", function(d) { 
+		return overviewX(d.report_time); 
 	})
-	.attr("x", function(d) {
-		return miniX(d.time);
+	.attr("cy", function(d) { 
+		return overviewY(d.report_type + .5); 
 	})
-	.attr("y", function(d) {
-		return miniY(d.type + .5) - 5;
-	})
-	.attr("width", miniRect.width)
-	.attr("height", miniRect.height)
 	.style("fill", function(d) {
-		return reportColor(d.type);
+		return reportColor(d.report_type);
 	});
 
-//mini labels
-mini.append("g").selectAll(".miniLabels")
-	.data(items)
-	.enter().append("text")
-	.text(function(d) {
-		return d.id;
+context.append("g")
+    .attr("class", "axis x-axis")
+    .attr("transform", "translate(0," + overviewHeight + ")")
+    .call(overviewXAxis);
+
+context.append("g")
+    .attr("class", "brush")
+    .call(brush)
+    .call(brush.move, x.range());
+
+
+//create brush function redraw scatterplot with selection
+function brushed() {
+	if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return; // ignore brush-by-zoom
+	var s = d3.event.selection || overviewX.range();
+	x.domain(s.map(overviewX.invert, overviewX));
+
+	focus.selectAll(".message")
+	.attr("cx", function(d) { 
+		return x(d.report_time); 
 	})
-	.attr("x", function(d) {
-		return miniX(d.time);
+	.attr("cy", function(d) { 
+		return mainY(d.report_type) + 10; 
 	})
-	.attr("y", function(d) {
-		return miniY(d.type + .5);
-	})
-	.attr("dy", ".5ex");
-
-// Creates a new one-dimensional brush along the x-dimension.
-var brush = d3.brushX()
-                // sets the brushable extent to the specified array of points [[x0, y0], [x1, y1]]
-                .extent([[0, 0], [w, miniHeight]])
-                // sets the event listener for "brush" type ("start", "brush" or "end") and returns the brush
-				.on("brush", showBrushedDetails); 
-
-// Brush overlay
-mini.append("g")
-	.attr("class", "brush")
-	.call(brush)
-	.selectAll("rect")
-	.attr("y", 1)
-	.attr("height", miniHeight - 1);
-
-
-function showBrushedDetails() {
-	/*
-		The brush attributes are no longer stored 
-		in the brush itself, but rather in the 
-		element it is brushing. That's where much of
-		the confusion around v4's brushes seems to be.
-		The new method is a little difficult to adapt
-		to, but seems more efficient. I think much of
-		this confusion comes from the fact that 
-		brush.extent() still exists, but means
-		something completely different.
-
-		Instead of calling brush.extent() to get the 
-		range of the brush, call 
-		d3.brushSelection(node) on what is being 
-		brushed.
-
-		brush.extent()[0] --> d3.brushSelection(this)[0];
-		brush.extent()[1] --> d3.brushSelection(this)[1];
-	*/
-	// For an x-brush, d3.brushSelection(this) is [x0, x1]
-	// But this [x0, x1] is the actual selection range, we'll need to 
-	// convert it back to the miniX range so we can use it to filter
-	// selected items based on item.start, item.end
-	var selectionRange = d3.brushSelection(this).map(miniX.invert);
-	var minExtent = selectionRange[0];
-	var maxExtent = selectionRange[1];
-console.log(minExtent + ", " + maxExtent);
-    // Filter out the brushed items based on selection
-	var brushedItems = items.filter(function(d) {
-		return (d.time + miniRect.width) > minExtent && d.time < maxExtent;
+	.style("fill", function(d) {
+		return reportColor(d.report_type);
 	});
 
-    // Set the domain to the specified array of values
-	mainX.domain([minExtent, maxExtent]);
+	focus.select(".x-axis").call(xAxis);
 
-	// update main item rects in main area
-	var rects = itemRects.selectAll("rect")
-	    .data(brushedItems, function(d) { 
-	    	return d.id; 
-	    })
-		.attr("x", function(d) {
-			return mainX(d.time);
-		})
-		.attr("width", function(d) {
-			// Need to miniX.invert(miniRect.width)
-			return mainX(d.time + miniX.invert(miniRect.width)) - mainX(d.time);
-		});
-	
-	rects.enter().append("rect")
-		.attr("class", function(d) {
-			return "miniItem" + d.type;
-		})
-		.attr("x", function(d) {
-			return mainX(d.time);
-		})
-		.attr("y", function(d) {
-			return mainY(d.type) + 10;
-		})
-		.attr("width", function(d) {
-			// Need to miniX.invert(miniRect.width)
-			return mainX(d.time + miniX.invert(miniRect.width)) - mainX(d.time);
-		})
-		.attr("height", function(d) {
-			// .8 of the mainY height to have some margins between two lanes
-			return .8 * mainY(1);
-		})
-		.style("fill", function(d) {
-			return reportColor(d.type);
-		});
-
-	rects.exit().remove();
-
-	// update the item labels
-	var labels = itemRects.selectAll("text")
-		.data(brushedItems, function (d) { 
-			return d.id; 
-		})
-		.attr("x", function(d) {
-			return mainX(Math.max(d.time, minExtent) + 2);
-		});
-
-	labels.enter().append("text")
-		.text(function(d) {
-			return d.id;
-		})
-		.attr("x", function(d) {
-			return mainX(Math.max(d.time, minExtent));
-		})
-		.attr("y", function(d) {
-			return mainY(d.type + .5);
-		})
-		.attr("text-anchor", "start");
-
-	labels.exit().remove();
+	svg.select(".zoom").call(zoom.transform, d3.zoomIdentity
+		.scale(width / (s[1] - s[0]))
+		.translate(-s[0], 0));
 }
 
+function zoomed() {
+	if (d3.event.sourceEvent && d3.event.sourceEvent.type === "brush") return; // ignore zoom-by-brush
+	var t = d3.event.transform;
+	x.domain(t.rescaleX(overviewX).domain());
+	focus.selectAll(".message")
+	.attr("cx", function(d) { 
+		return x(d.report_time); 
+	})
+	.attr("cy", function(d) { 
+		return mainY(d.report_type + .5); 
+	});
+
+	focus.select(".x-axis").call(xAxis);
+	context.select(".brush").call(brush.move, x.range().map(t.invertX, t));
+}
