4ca45da1d1f43d7f176f4c9710a9a1735a15032e
yuanzhou <yuanzhou19@gmail.com>
2018-06-20 16:33:38 -0400

    Adding biomarkers chart - in progress

--- client/js/deepphe.js
+++ client/js/deepphe.js
@@ -358,7 +358,6 @@ function showStagesChart(svgContainerId, data) {
 			return d + " (" + patientsCounts[d] + ")";
 		});
 		
-
 }
 
 // Filter the patients by given cancer stage
@@ -397,6 +396,111 @@ function showPatients(stage) {
 	});
 }
 
+function showBiomarkersChart(svgContainerId, data) {
+    console.log(data);
+
+	var margin = {top: 20, right: 20, bottom: 30, left: 60};
+	var width = 600 - margin.left - margin.right;
+	var height = 540 - margin.top - margin.bottom;
+
+    var svg = d3.select("#" + svgContainerId).append("svg")
+	    .attr("class", "biomarker_chart")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+		    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var keys = ['positive', 'negative', 'unknown'];
+
+    // Outer x axis
+	var x0 = d3.scaleBand()
+	    .domain(data.biomarkersPool)
+	    .rangeRound([0, width]);
+
+    // Inner x axis
+	var x1 = d3.scaleBand()
+	    .domain(keys)
+	    .rangeRound([0, x0.bandwidth()])
+	    .padding(0.05);
+
+	var y = d3.scaleLinear()
+	    .rangeRound([height, 0])
+	    .domain([0, 100]);
+
+    var z = d3.scaleOrdinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888"]);
+
+
+	svg.append("g")
+		.selectAll("g")
+		.data(data.data)
+		.enter().append("g")
+		.attr("transform", function(d) { 
+			return "translate(" + x0(d.biomarker) + ",0)"; 
+		})
+		.selectAll("rect")
+		.data(function(d) { 
+			return keys.map(function(key) { 
+			    return {key: key, value: d[key]}; 
+			}); 
+		})
+		.enter().append("rect")
+		.attr("x", function(d) { 
+			console.log(d);
+			return x1(d.key); 
+		})
+		.attr("y", function(d) { 
+			return y(d.value); 
+		})
+		.attr("width", x1.bandwidth())
+		.attr("height", function(d) { 
+			return height - y(d.value); 
+		})
+		.attr("fill", function(d) { 
+			return z(d.key); 
+		});
+
+    // X axis, outer
+	svg.append("g")
+		.attr("class", "axis")
+		.attr("transform", "translate(0," + height + ")")
+		.call(d3.axisBottom(x0));
+
+    // Y axis
+	svg.append("g")
+		.attr("class", "axis")
+		.call(d3.axisLeft(y))
+		.append("text")
+			.attr("x", 2)
+			.attr("y", y(y.ticks().pop()) + 0.5)
+			.attr("dy", "0.32em")
+			.attr("fill", "#000")
+			.attr("font-weight", "bold")
+			.attr("text-anchor", "start")
+			.text("Percentage");
+
+    var legend = svg.append("g")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 10)
+      .attr("text-anchor", "end")
+    .selectAll("g")
+    .data(keys.slice().reverse())
+    .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+  legend.append("rect")
+      .attr("x", width - 19)
+      .attr("width", 19)
+      .attr("height", 19)
+      .attr("fill", z);
+
+  legend.append("text")
+      .attr("x", width - 24)
+      .attr("y", 9.5)
+      .attr("dy", "0.32em")
+      .text(function(d) { return d; });
+}
+
 function getPatientsTumorInfo(patientNames) {
     // Separate the ajax request with callbacks
 	var jqxhr = $.ajax({
@@ -407,10 +511,9 @@ function getPatientsTumorInfo(patientNames) {
 	});
 
 	jqxhr.done(function(response) {
-	    console.log(response);
+	    //console.log(response);
 
-	    // Render response
-	    //$('#tumorchart').html(response);
+	    showBiomarkersChart("biomarkers", response.biomarkersInfo);
 	});
 
 	jqxhr.fail(function () { 
